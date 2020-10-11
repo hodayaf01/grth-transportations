b143780157b2c34fc63a76d6f03cda5201bcb3b4
hodaya.farkash@gmail.com <hodaya.farkash@gmail.com>
2020-10-11 23:35:59 +0300

    add basic popup

---
+++ app/components/AddressPopup/index.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import Popup from 'reactjs-popup';
+
+import './index.scss';
+
+
+function AddressPopup() {
+  const [seen, setSeen] = useState(true);
+
+  const togglePop = () => {
+    setSeen(false)
+  };
+
+  return (
+    <Popup 
+      open={seen}
+      closeOnDocumentClick 
+      onClose={togglePop}
+      position="right center"
+    >
+      <div className="modal">
+        <button className="close" type="button" onClick={togglePop}> &times; </button>
+        {/* <a className="close" onClick={togglePop}> &times; </a> */}
+      </div>
+      <div className="try">Popup content here !!</div>
+    </Popup>
+  );
+}
+
+
+export default (AddressPopup);
