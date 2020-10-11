dc039eced57fc95c827e45662dafe35d593ab0d2
hodaya.farkash@gmail.com <hodaya.farkash@gmail.com>
2020-10-11 23:42:11 +0300

    change transportationForm to class + CSS

--- app/components/AddTransportationForm/index.js
+++ app/components/AddTransportationForm/index.js
@@ -1,48 +1,78 @@
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
+import { func } from 'prop-types';
+import React from 'react';
+import Geocode from "react-geocode";
+import './index.scss';
 
-function AddTransportationForm(props){
+class AddTransportationForm extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      customerId: '',
+      name: '',
+      fromlat: '',
+      fromlng: '',
+      tolat: '',
+      tolng: '',
+      from: '',
+      to:'',
+    }
+  }
 
-  const [customerId, setCustomerId] = useState(0);
-  const [name, setName] = useState('');
-  const [address, setAddress] = useState('');
-  const [from, setFrom] = useState('');
-  const [to, setTo] = useState('');
+  render(){
+    const {newId, submitAddNewTransportation} = this.props;
 
-  const onCustomerIdChanged = (event) => {
-    setCustomerId(event.target.value);
-  };
-  const onNameChanged = (event) => {
-    setName(event.target.value);
-  };
-  const onAddressChanged = (event) => {
-    setAddress(event.target.value);
-  };
-  const onFromChanged = (event) => {
-    setFrom(event.target.value);
-  };
-  const onToChanged = (event) => {
-    setTo(event.target.value);
-  };
+    const onCustomerIdChanged = (event) => this.setState({customerId:event.target.value }) 
+    const onNameChanged = (event) => this.setState({name: event.target.value})
+    const onFromChanged = (event) => this.setState({from: event.target.value})
+    const onToChanged = (event) => this.setState({to: event.target.value})
 
-  return (
-    <div>
-      <form>
-        <span>Transportation Number:</span> <span>{props.newId}</span><br/>
-        <span>Customer Number</span> <input placeholder="Customer Number" value={customerId} onChange={onCustomerIdChanged}></input><br/>
-        <span>Name</span> <input placeholder="Name" value={name} onChange={onNameChanged}></input><br/>
-        <span>Address</span> <input placeholder="Address" value={address} onChange={onAddressChanged}></input><br/>
-        <span>From</span> <input placeholder="From" value={from} onChange={onFromChanged}></input><br/>
-        <span>To</span> <input placeholder="To" value={to} onChange={onToChanged}></input><br/>
-  
-        <input type="submit"></input>
-      </form>
-    </div>
-  );
+    const onSubmitAddForm = () => {
+
+      getAddress(this.state.from, 'from');
+      getAddress(this.state.to, 'to');
+
+      const tempTrans = {
+        customerId: this.state.customerId,
+        name: this.state.name,
+        fromlat: this.state.fromlat,
+        fromlng: this.state.fromlng,
+        tolat: this.state.tolat,
+        tolng: this.state.tolng
+      };
+      submitAddNewTransportation(tempTrans);
+    };
+
+
+    const getAddress = (address, type) => {
+      Geocode.setApiKey("AIzaSyBp92tTnTQgEpN230RfIsFcPW73YDyC1sM");
+      Geocode.enableDebug();
+      Geocode.fromAddress(address).then(
+        response => {
+          if(response.status === 'OK'){
+            const { lat, lng } = response.results[0].geometry.location;
+            if(type === 'from') {this.setState({fromlat: lat}); this.setState({fromlng: lng})}
+            else {this.setState({tolat: lat}); this.setState({tolng: lng})}
+          }
+        },
+        error => {
+          console.error(error);
+        }
+      );    
+    };
+
+    return(
+      <div>
+        <form>
+          <p>Transportation Number:</p> <span>{newId}</span><br/>
+          <p>Customer Number</p> <input placeholder="Customer Number" value={this.state.customerId} onChange={onCustomerIdChanged}></input><br/>
+          <p>Name</p> <input placeholder="Name" value={this.state.name} onChange={onNameChanged}></input><br/>
+          <p>From</p> <input placeholder="From" value={this.state.from} onChange={onFromChanged}></input><br/>
+          <p>To</p> <input placeholder="To" value={this.state.to} onChange={onToChanged}></input><br/>
+          <button className="submitButton" type='button' onClick={onSubmitAddForm}>submit</button>
+        </form>
+      </div>
+    );
+  }
+
 }
-
-AddTransportationForm.prototype={
-  newId: PropTypes.number,
-};
-
-export default (AddTransportationForm);
+export default AddTransportationForm;
