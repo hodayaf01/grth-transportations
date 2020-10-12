9d4a4655f144ce518404b3ade5420ec531eb6130
hodaya.farkash@gmail.com <hodaya.farkash@gmail.com>
2020-10-12 22:45:05 +0300

    update header with logout

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -10,7 +10,7 @@ function Header(){
       <ul>
         <li className="item"><a  href="/transportaionList">Transportation List</a></li>
         <li  className="item"><a  href="/addTrasportaion">Add new Transportation</a></li>
-        <li className="item LoggedName"><a href="/">Hi {JSON.parse(localStorage.getItem('userName'))}</a></li>
+        <li className="item LoggedName"><a href="/">Hi {JSON.parse(localStorage.getItem('userName'))} - Logout</a></li>
       </ul>
     </div>
   );
