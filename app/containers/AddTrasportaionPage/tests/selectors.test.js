841c844f73439f789e82c911b7431d3931a182f0
hodaya.farkash@gmail.com <hodaya.farkash@gmail.com>
2020-10-13 18:47:52 +0300

    remove unnececary code

--- app/containers/AddTrasportaionPage/tests/selectors.test.js
+++
@@ -1,7 +0,0 @@
-// import { selectAddTrasportaionPageDomain } from '../selectors';
-
-describe('selectAddTrasportaionPageDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});
